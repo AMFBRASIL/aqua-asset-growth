1584f30f1d6f45a7e9cfd8912aa77ff71085baa3
Anderson Mautone <promautone@gmail.com>
2026-04-07 21:53:47 -0300

    AtualizandoVersao

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="pt-BR">
-      <body>
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body suppressHydrationWarning>
         <Providers>{children}</Providers>
       </body>
     </html>
